bb61b3923d96cf7a346724a3e742bc824d976419
standupright <55686549+standupright@users.noreply.github.com>
2021-02-24 11:15:22 +0500

    Удаление свитчей

--- js/form.js
+++ js/form.js
@@ -1,51 +1,28 @@
-const form = document.querySelector ('.map__filters');
-const housingType = form.querySelector ('#housing-type');
-const price = document.querySelector ('#price');
-const timein = document.querySelector ('#timein');
-const timeout = document.querySelector ('#timeout');
+const form = document.querySelector('.map__filters');
+const housingType = form.querySelector('#housing-type');
+const price = document.querySelector('#price');
+const timein = document.querySelector('#timein');
+const timeout = document.querySelector('#timeout');
+
+const TYPE_PRICES = {
+  'flat': 1000,
+  'bungalow': 0,
+  'house': 5000,
+  'palace': 10000,
+}
 
 const onHousingTypeClick = function () {
-  switch (this.value) {
-    case 'palace':
-      price.min = 10000;
-      price.placeholder = 10000;
-      break;
-    case 'flat':
-      price.min = 1000;
-      price.placeholder = 1000;
-      break;
-    case 'house':
-      price.min = 5000;
-      price.placeholder = 5000;
-      break;
-    case 'bungalow':
-      price.min = 0;
-      price.placeholder = 0;
-      break;
-    default:
-      break;
-  }
+  price.min =  TYPE_PRICES[`${this.value}`];
+  price.placeholder = TYPE_PRICES[`${this.value}`];
 };
 
-housingType.addEventListener ('change', onHousingTypeClick);
+housingType.addEventListener('change', onHousingTypeClick);
 
 const onTimeClick = function (changeOut) {
   return function () {
-    switch (this.value) {
-      case '12:00':
-        changeOut.value = '12:00';
-        break;
-      case '13:00':
-        changeOut.value = '13:00';
-        break;
-      case '14:00':
-        changeOut.value = '14:00';
-        break;
-      default:
-        break;
-    }
+    changeOut.value = this.value;
   };
 };
 
-timein.addEventListener ('change', onTimeClick (timeout));
-timeout.addEventListener ('change', onTimeClick (timein));
+timein.addEventListener('change', onTimeClick(timeout));
+timeout.addEventListener('change', onTimeClick(timein));
